d5a6716c1ace2b7925dfcc2aec47a36f7f806ebd
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-12-16 17:24:58 +0100

    [UPD] used js functions and manipulated DOM to create all boxes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
 import { source } from './source.js';
 
-console.log(source);
+function  createMiniBox() {
+    let currentElement = document.createElement('div');
+
+    currentElement.classList.add('box');
+
+    return currentElement
+};
+
+function createChristmasBox() {
+    let currentElement = document.createElement('div');
+
+    currentElement.classList.add('christmas_box');
+
+    return currentElement
+}
+
+const calendar = document.getElementById('calendar');
+
+const christmasBox = document.getElementById('christmasBox')
+
+for (let i = 0; i < 24; i++) {
+
+    let calendarBox = createMiniBox();
+
+    calendarBox.innerText = i + 1;
+
+    calendar.append(calendarBox);
+};
+
+let calendarChristmasBox = createChristmasBox();
+
+calendarChristmasBox.innerText = 25
+
+christmasBox.append(calendarChristmasBox);
