1b905ffbf4f12584f2f7dbeae23d5572f41ffb29
AleFarinaIV <Alessandrofarina401@gmail.com>
2025-01-07 19:12:17 +0100

    [UPD] updated modals concept

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 import { source } from './source.js';
 
-function  createBox() {
+function  createBox(index) {
     let currentElement = document.createElement('div');
 
     currentElement.classList.add('clickable', 'mini_box');
 
     currentElement.setAttribute('data-bs-toggle', 'modal');
     
-    currentElement.setAttribute('data-bs-target', '#exampleModal');
+    currentElement.setAttribute('data-bs-target', `#modal-${index}`);
 
     return currentElement
 };
@@ -15,16 +15,36 @@ function  createBox() {
 const calendar = document.getElementById('calendar');
 
 for (let i = 0; i < 25; i++) {
+    const modalId = `modal-${i}`;
 
-    let calendarBox = createBox();
+    const boxId = `box-${i}`;
+
+    const modal_content = source[i].type === 'text' ? `<h2>${source[i].text}</h2>` : `<img src="../${source[i].url}" alt="${source[i].type}"`;
+
+    let calendarBox = createBox(i);
 
     let boxContent = document.createElement('div');
+
+    boxContent.setAttribute('id', `${boxId}`)
 
     boxContent.classList.add('box_content');
 
     boxContent.innerHTML = `
         <img src="../images/icons/${source[i].icon}.png" alt="${source[i].icon}">
         <span class='fw-bold'>${i+1}</span>
+
+        <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+              <div class="modal-content">
+                <div class="modal-body p-0 pt-3 bg-rose">
+                    ${modal_content}
+                </div>
+                <div class="modal-footer bg-rose justify-content-center border-0">
+                  <button type="button" class="bg-primary text-white py-1 px-2" data-bs-dismiss="modal">CHIUDI</button>
+                </div>
+              </div>
+            </div>
+        </div>
     `;
 
     calendarBox.append(boxContent);
